c24b691703cf100bacbb378e8b6854d59d7fdc84
BenjaJuarez059 <tbjuarez@escuelasproa.edu.ar>
2025-12-22 18:41:13 -0300

    carrusel con nuestros proyectos

--- script.js
+++ script.js
@@ -2,3 +2,31 @@ function toggleMenu() {
     const menu = document.getElementById("menu");
     menu.style.display = menu.style.display === "flex" ? "none" : "flex";
 }
+let current = 0;
+
+const slides = document.querySelectorAll(".slide");
+const title = document.getElementById("projectTitle");
+
+const projectNames = [
+  "AR-mate",
+  "Sparky",
+  "ConCiencia Colectiva"
+];
+
+function showSlide(index) {
+  slides.forEach(slide => slide.classList.remove("active"));
+  slides[index].classList.add("active");
+
+
+  title.textContent = projectNames[index];
+}
+
+document.querySelector(".next").onclick = () => {
+  current = (current + 1) % slides.length;
+  showSlide(current);
+};
+
+document.querySelector(".prev").onclick = () => {
+  current = (current - 1 + slides.length) % slides.length;
+  showSlide(current);
+};
